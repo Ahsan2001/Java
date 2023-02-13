cd58293b504f22282eca83e7e830ce8726085eb5
M. Ahsan Shaikh <ahsan2sabir@gmail.com>
2023-02-13 23:26:26 +0500

    Create Compilation.java

---
+++ Learning-Example/Compilation.java
@@ -0,0 +1,15 @@
+// A file called Plankton.java, we could compile it with the terminal command:
+
+
+// javac Plankton.java
+
+
+
+
+// A successful compilation produces a .class file: Plankton.class, that we execute with the terminal command:
+
+// java Plankton
+
+
+
+// An unsuccessful compilation produces a list of errors. No .class file is made until the errors are corrected and the compile command is run again.
