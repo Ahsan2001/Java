169cf1f54cd28b9c9844448078fda3cb441eec92
M. Ahsan Shaikh <ahsan2sabir@gmail.com>
2023-02-13 23:01:13 +0500

    print methods
    two method for output in console

---
+++ Learning-Example/consolePrint.java
@@ -0,0 +1,8 @@
+// System.out.println() and System.out.print() are two methods in Java used for printing output to the console. 
+// The main difference between these two methods is the line ending:
+
+// System.out.println(): 
+// The println method adds a line break at the end of the output, so the next output will be printed on a new line.
+
+// System.out.print(): 
+// The print method does not add a line break at the end of the output, so the next output will be printed on the same line.
