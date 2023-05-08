97567f55a8c557f52f74e6b3ef610c3f07d2733b
M. Ahsan Shaikh <ahsan2sabir@gmail.com>
2023-05-08 19:51:57 +0500

    Update twoDimensitonArray.java

--- Presentation/twoDimensitonArray.java
+++ Presentation/twoDimensitonArray.java
@@ -1,16 +1,26 @@
 class MultidimensionalArray {
     public static void main(String[] args) {
 
-        // create a 2d array
+    //  create a 2d array
+        
+    //  dataType[] arrayName 
         int[][] a = {
             {1, 2, 3}, 
             {4, 5, 6, 9}, 
             {7}, 
         };
       
-        // calculate the length of each row
+        // calculate the length array 
         System.out.println("Length of row 1: " + a[0].length);
-        System.out.println("Length of row 2: " + a[1].length);
-        System.out.println("Length of row 3: " + a[2].length);
+
+        
+        
+        // specific value print 
+        arr[0][0] = 1
+        arr[0][1] = 2
+        arr[1][0] = 4
+        arr[1][1] = 4
+
+        
     }
 }
